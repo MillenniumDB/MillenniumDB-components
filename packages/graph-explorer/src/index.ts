0b5fd0267262248b07db2a05fe5279d2ace1f44f
VicenteVicente <vicente.calisto98@gmail.com>
2025-08-12 17:17:08 -0300

    export graphAPI

--- packages/graph-explorer/src/index.ts
+++ packages/graph-explorer/src/index.ts
@@ -2,3 +2,4 @@ export type { GraphColorConfig } from "./constants/colors";
 export { GraphExplorer, type GraphExplorerAPI as GraphExplorerHandle } from "./graph-explorer";
 export type { MDBGraphData, MDBGraphLink, MDBGraphNode } from "./types/graph";
 export { MDBGraphExplorer, type MDBGraphExplorerProps } from "./mdb-graph-explorer";
+export { useGraphAPI } from "./hooks/use-graph-api";
