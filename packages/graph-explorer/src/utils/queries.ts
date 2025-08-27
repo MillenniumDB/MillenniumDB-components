6296bccc3deeed7e0bc43efca831c83c36889516
VicenteVicente <vicente.calisto98@gmail.com>
2025-08-27 13:22:41 -0300

    handle no search properties set

--- packages/graph-explorer/src/utils/queries.ts
+++ packages/graph-explorer/src/utils/queries.ts
@@ -2,8 +2,9 @@ import type { NodeId } from "../types/graph";
 
 export const getFetchNodesQuery = (query: string, properties: string[], limit: number = 50): string => {
   const orConditions = properties.map((prop) => `REGEX(?node.${prop},"(^|\\s)(${query}).*","i")`).join(" OR ");
+  const whereStatement = orConditions.length > 0 ? `WHERE ${orConditions}` : "";
   return `MATCH (?node)
-WHERE ${orConditions}
+${whereStatement}
 RETURN *
 LIMIT ${limit}`;
 };
